0139b2252577c07eb001976c96225591bfe7391e
Bassline32 <ivandzen911@Gmail.com>
2026-02-22 19:33:56 +0300

    начал делать бд

--- src/main/java/com/example/url_shortener/entity/ShortUrlEntity.java
+++ src/main/java/com/example/url_shortener/entity/ShortUrlEntity.java
@@ -1,28 +1,34 @@
 package com.example.url_shortener.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Entity
 @Table(name = "short_url")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ShortUrlEntity {
 
     @Id
-    @Column(name = "short_code", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "short_code", nullable = false, unique = true, length = 20)
     // "abc123"
     private String shortCode;
 
-    @Column(name = "original_url", nullable = false)
+    @Column(name = "original_url", nullable = false, length = 2048)
     // "https://example.com/very/long/path"
     private String originalUrl;
 
-    @Column(name = "created_at", nullable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     //используется для хранения даты и времени,
     // когда была создана сокращённая ссылка.
     private LocalDateTime createdAt;
@@ -31,7 +37,7 @@ public class ShortUrlEntity {
     // nullable — может быть бессрочной
     private LocalDateTime expiresAt = LocalDateTime.now().plusDays(20);
 
-    @Column(name = "click_count")
+    @Column(name = "click_count", nullable = false)
     //счётчик переходов по ссылке
     private Integer clickCount = 0;
 
---
+++ src/main/java/com/example/url_shortener/entity/User.java
@@ -0,0 +1,55 @@
+package com.example.url_shortener.entity;
+
+import com.example.url_shortener.model.ShortUrl;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    //связь oneToMany с  ссылками
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Builder.Default
+    private List<ShortUrl> urls = new ArrayList<>();
+
+    //связь oneToMany с папками
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Builder.Default
+    private List<Folder> folders = new ArrayList<>();
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+
+}
+
