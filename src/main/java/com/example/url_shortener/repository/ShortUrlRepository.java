5957b1a60127cc682b1a70a83ae854e7d212b5f3
Bassline32 <ivandzen911@Gmail.com>
2026-02-27 19:13:23 +0300

    7.3.1 IN DEV (1)

--- src/main/java/com/example/url_shortener/repository/ShortUrlRepository.java
+++ src/main/java/com/example/url_shortener/repository/ShortUrlRepository.java
@@ -2,6 +2,8 @@ package com.example.url_shortener.repository;
 
 import com.example.url_shortener.model.ShortUrl;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,9 +21,9 @@ public interface ShortUrlRepository extends JpaRepository<ShortUrl, Long> {
     //ссылки пользователя
     List<ShortUrl> findByUserIdOrderByCreatedAtDesc(Long userId);
 
-
-
-
+    //поиск по домену (JPQL)
+    @Query("SELECT s FROM ShortUrlEntity s WHERE s.originalUrl LIKE %:domain%")
+    List<ShortUrl> findByDomain(@Param("domain") String domain);
 
 
 }
