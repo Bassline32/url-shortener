fa9d56341c19655a1a809adc5c09efcf25dc90b1
Bassline32 <ivandzen911@Gmail.com>
2026-03-16 20:41:17 +0300

    POST API V1 FOLDERS(DONE)

--- src/main/java/com/example/url_shortener/controller/FolderController.java
+++ src/main/java/com/example/url_shortener/controller/FolderController.java
@@ -5,6 +5,7 @@ import com.example.url_shortener.dto.response.CreateFolderResponse;
 import com.example.url_shortener.repository.FolderRepository;
 import com.example.url_shortener.service.FolderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,8 +26,6 @@ public class FolderController {
 
         CreateFolderResponse response = folderService.createFolder(request.name(), request);
 
-
-
-
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
